dbc24bcc8e99d545356379b9a1b2627f304004cc
TXS81 <ihr2015.16@gmail.com>
2018-05-29 11:27:59 +0300

    Added script

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('.container-fluid').each(function () {
+        var highestBox = 0;
+        $('.col-sm-4', this).each(function () {
+            if ($(this).height() > highestBox) {
+                highestBox = $(this).height();
+            }
+        });
+        $('.col-sm-4', this).height(highestBox);
+        
+    });
+});
